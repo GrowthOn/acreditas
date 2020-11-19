0bbf7483250ce5b15a9e2d15e2780786b7354bbf
Matheus Mitestainer <matheusmitestainer@outlook.com>
2020-11-18 23:10:49 -0300

    Add menu and footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,33 @@
 const form = document.querySelector('form')
-const fields = [...document.querySelectorAll('input')]
-const button = document.querySelector('button')
+const fields = [...document.querySelectorAll('input')].filter(field => field.type !== 'hidden')
+const formButton = document.querySelector('form button')
+const header = document.querySelector('header')
+const logo = document.querySelector('#logo')
+const menuItems = [...document.querySelectorAll('nav ul li')]
 
 window.onload = () => {
-    fields.filter(field => field.type !== 'hidden').forEach(field => field.value = '')
-    button.disabled = true
+    fields.forEach(field => field.value = '')
 }
 
-fields.forEach(field => {
-    field.addEventListener('input', () => button.disabled = !form.checkValidity())
+[logo, ...menuItems].forEach(item => item.addEventListener('click', () => {
+    zenscroll.toY(0)
+    menu.classList.toggle('menu-closed')
+    body.removeEventListener('touchstart', closeMenu)
+}))
+
+const body = document.querySelector('body')
+const nav = document.querySelector('nav')
+const menu = document.querySelector('nav ul')
+const toggleButton = document.querySelector('#menu-toggle')
+
+toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('menu-closed')
+    body.addEventListener('touchstart', closeMenu)
 })
+
+const closeMenu = e => {
+    if (e.target !== nav && menuItems.indexOf(e.target) === -1 && e.target !== toggleButton) {
+        menu.classList.toggle('menu-closed')
+        body.removeEventListener('touchstart', closeMenu)
+    }
+}
